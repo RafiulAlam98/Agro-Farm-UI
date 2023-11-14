06c4572fc11440d9c3403253bb3fc109d265ef4b
RafiulAlam <mrafiul.alam7@gmail.com>
2023-11-14 13:09:35 +0600

    Initialize project

---
+++ src/app/core/db/carousel.db.ts
@@ -0,0 +1,30 @@
+import {Carousel} from "../../interfaces/common/carousel.interface";
+
+export const ALL_CAROUSEL: Carousel[] = [
+  {
+    "_id": "1",
+    "title": "Eid Sale",
+    "image": "https://img.freepik.com/free-photo/cows-eating-lush-grass-green-field-front-fuji-mountain-japan_335224-36.jpg?w=740&t=st=1699944966~exp=1699945566~hmac=f43928f9c24e6d9ae31bc962c3b8be8d138456f7987301716362f257d3c41ef7",
+  },
+  {
+    "_id": "2",
+    "title": "Eid Sale",
+    "image": "https://img.freepik.com/free-photo/close-up-milk-from-goats_23-2148673024.jpg?size=626&ext=jpg&ga=GA1.1.16233275.1687409673&semt=ais",
+  },
+  {
+    "_id": "3",
+    "title": "Eid Sale",
+    "image": "https://img.freepik.com/premium-photo/nelore-cattle-herd-sunny-day_146714-1605.jpg?size=626&ext=jpg&ga=GA1.1.16233275.1687409673&semt=ais",
+  },
+  {
+    "_id": "4",
+    "title": "Eid Sale",
+    "image": "https://img.freepik.com/premium-photo/group-holstein-cows-pasture_420555-101.jpg?size=626&ext=jpg&ga=GA1.1.16233275.1687409673&semt=ais",
+  },
+  {
+    "_id": "5",
+    "title": "Eid Sale",
+    "image": "https://img.freepik.com/free-photo/closeup-shot-white-cow-grazing-pasture_181624-48705.jpg?size=626&ext=jpg&ga=GA1.1.16233275.1687409673&semt=ais",
+  },
+];
+
